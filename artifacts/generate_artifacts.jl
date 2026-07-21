0704375b679c5c904376f289ee7de074bedf844d
Ivan <iv.borissov@gmail.com>
2026-07-22 00:42:31 +0300

    heta-compiler artifacts upd

--- artifacts/generate_artifacts.jl
+++ artifacts/generate_artifacts.jl
@@ -1,7 +1,7 @@
 using ArtifactUtils
 using Pkg.Artifacts
 
-const HETA_COMPILER_RELEASE = "v0.12.0"
+const HETA_COMPILER_RELEASE = "v0.12.1-beta.0"
 
 const artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
